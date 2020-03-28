f8c578a77aba85f7e753d1ae3f35dd9036e19138
Rany <ranyel@msn.com>
2020-03-27 23:57:20 -0700

    added template

--- sdk/src/index.tsx
+++ sdk/src/index.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { ThemeProvider } from 'styled-components'
 
-import { unregister } from './Core/service-worker';
+import { unregister } from './core'
+import { GlobalStyles, theme } from './styles'
 
-ReactDOM.render(<div>Rany ElHousieny</div>, document.getElementById('root'));
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <GlobalStyles />
+    <div>Hello World</div>
+  </ThemeProvider>,
+  document.getElementById('root')
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-unregister();
+unregister()
